9e73f72b070d87ee77a9503ff2995b49e66aa6c7
SusanaArtime <susartime@gmail.com>
2024-11-17 02:56:43 +0100

    subo el modelo RnaTranscripcion.java y la App.java terminados

--- src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
+++ src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
@@ -4,7 +4,33 @@ package org.factoriaf5.rna_transcription;
 public class RnaTranscription {
     
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Implement method");
-    }
 
+        StringBuilder rna = new StringBuilder();
+
+        for (int i = 0; i < dnaStrand.length(); i++) {
+            char nucleotide = dnaStrand.charAt(i);
+
+            switch (nucleotide) {
+                case 'G':
+                    rna.append('C');
+                    break;
+                case 'C':
+                    rna.append('G');
+                    break;
+                case 'T':
+                    rna.append('A');
+                    break;
+                case 'A':
+                    rna.append('U');
+                    break;
+                default:
+                    throw new IllegalArgumentException("Secuencia de ADN inválida");
+            }
+        }
+
+        return rna.toString();
+    }
+        
 }
+
+
